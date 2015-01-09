5006224bc9f220d4bbd0023922b8a9f6871b9456
Andraz <andraz.pajtler@klika.si>
2015-01-09 17:20:38 +0100

    Admin login

--- src/test/java/at/ac/tuwien/inso/swtesten/sportsid/AdminLoginStepDefinitions.java
+++ src/test/java/at/ac/tuwien/inso/swtesten/sportsid/AdminLoginStepDefinitions.java
@@ -1,8 +1,12 @@
 package at.ac.tuwien.inso.swtesten.sportsid;
 
+import javax.swing.Box.Filler;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
+import com.sun.jna.platform.win32.WinNT.WELL_KNOWN_SID_TYPE;
 
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
@@ -14,35 +18,31 @@ import static org.junit.Assert.assertEquals;
 public class AdminLoginStepDefinitions {
 	
 	private SportsIdSeleniumHelper sportsIdPage = new SportsIdSeleniumHelper();
-	private WebDriver driver = sportsIdPage.driver;
-	private String baseUrl = sportsIdPage.baseUrl;
 
 	@Given("^I want to log in with the user gruppe(\\d+)a$")
 	public void I_want_to_log_in_with_the_user_gruppe_a(int arg1) throws Throwable {
 	    sportsIdPage.setUp();
-	    driver.get(baseUrl + "/#pages.login");
+	   sportsIdPage.goToLoginPage();
 	}
 
 	@When("^I write gruppe(\\d+)a in the username field$")
 	public void I_write_gruppe_a_in_the_username_field(int arg1) throws Throwable {
-	    driver.findElement(By.id("input-username")).clear();
-	    driver.findElement(By.id("input-username")).sendKeys("gruppe27a");
+		sportsIdPage.fillUsernameField("gruppe27a");
 	}
 
 	@When("^When I write thomas(\\d+) in the password field$")
 	public void When_I_write_thomas_in_the_password_field(int arg1) throws Throwable {
-		driver.findElement(By.id("input-password")).clear();
-	    driver.findElement(By.id("input-password")).sendKeys("thomas1234");
+		sportsIdPage.fillPasswordField("thomas1234");
 	}
 
 	@When("^When I click the Login button$")
 	public void When_I_click_the_Login_button() throws Throwable {
-		driver.findElement(By.id("login-button")).click();
+		sportsIdPage.clickLoginButton();
 	}
 
 	@Then("^the welcome page is shown$")
 	public void the_welcome_page_is_shown() throws Throwable {
-		assertEquals(driver.findElement(By.cssSelector("h1")).getText(), "Welcome ADMIN A GRUPPE 27!"); 
+		sportsIdPage.welcomePageIsShown();
 	    sportsIdPage.shutDown();
 	}
 	
--- src/test/java/at/ac/tuwien/inso/swtesten/sportsid/SportsIdSeleniumHelper.java
+++ src/test/java/at/ac/tuwien/inso/swtesten/sportsid/SportsIdSeleniumHelper.java
@@ -20,6 +20,30 @@ public class SportsIdSeleniumHelper {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 	
+	public void goToLoginPage(){
+	    driver.get(baseUrl + "/#pages.login");
+	}
+	
+	public void fillUsernameField(String userName){
+	    driver.findElement(By.id("input-username")).clear();
+	    driver.findElement(By.id("input-username")).sendKeys(userName);
+	}
+	
+	public void fillPasswordField(String password){
+		driver.findElement(By.id("input-password")).clear();
+	    driver.findElement(By.id("input-password")).sendKeys(password);
+	}
+	
+	public void clickLoginButton(){
+		driver.findElement(By.id("login-button")).click();
+	}
+	
+	public void welcomePageIsShown(){
+		assertEquals(driver.findElement(By.cssSelector("#content-inner > div:nth-child(1) > h1:nth-child(1)")).getText(), "Welcome ADMIN A GRUPPE 27!");
+	}
+	
+	
+	
 	public void shutDown() {
 		SeleniumWebDriver.closeDriver();
 	}
